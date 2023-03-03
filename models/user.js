34c38ee483a8fe3a1032aa62661d720b3dcda82e
Folawewo Anthony <famwallexie@gmail.com>
2023-03-03 23:46:41 +0100

    user model

--- models/user.js
+++ models/user.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
     },
-    followers: [{}]
-})
+  ],
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+});
+
+module.exports = mongoose.model('User', userSchema);
