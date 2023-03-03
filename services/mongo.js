9a79c3993f3b4ddf2dfc1f799f9f41fe014385d7
Folawewo Anthony <famwallexie@gmail.com>
2023-03-04 00:44:28 +0100

    refactored database connection

--- services/mongo.js
+++ services/mongo.js
@@ -2,8 +2,26 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-mongoose.connect('process.env.MONGO_URL', () => {
+mongoose.connection.on('error', (err) => {
+  console.error(`Database Connection error: ${err}`);
+});
+
+mongoose.connection.on('connected', () => {
   console.log('Database Connected');
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Database Disconnected');
+});
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Database connection closed due to server termination');
+    process.exit(0);
+  });
 });
