fcb9666e77498bcde4504d8892f73b6b8f73cde7
Folawewo Anthony <famwallexie@gmail.com>
2023-03-04 22:34:59 +0100

    create post

---
+++ controllers/post.js
@@ -0,0 +1,25 @@
+const Post = require('../models/post');
+const User = require('../models/user');
+
+exports.createPost = (req, res) => {
+  const { text } = req.body;
+  const post = new Post({ text, user: req.user._id });
+  post.save((err, post) => {
+    if (err) {
+      res.status(500).json({ error: err });
+    } else {
+      User.findByIdAndUpdate(
+        req.user._id,
+        { $addToSet: { following: req.user._id } },
+        { new: true },
+        (err, user) => {
+          if (err) {
+            res.status(500).json({ error: err });
+          } else {
+            res.json(post);
+          }
+        }
+      );
+    }
+  });
+};
